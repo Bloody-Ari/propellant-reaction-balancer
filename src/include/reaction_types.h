886c5d3147145aa77ed29c8d45d476c2756940b7
Ari <alicewulfric@gmail.com>
2026-03-19 10:20:12 -0600

    Added some stuff, like density

--- src/include/reaction_types.h
+++ src/include/reaction_types.h
@@ -2,9 +2,15 @@ struct ReactionData {
   float fuel_g;
   float fuel_mol;
   float fuel_uma; /* g/mol */
+  float fuel_molarity; /* M = m/L, for liquids! */
+  float fuel_density_g_cm3; /* *1000 for kg/m^3 haha */
+  float fuel_volume; /* g/cm^3 or mL */
   float oxidizer_g;
+  float oxidizer_volume; 
   float oxidizer_mol;
   float oxidizer_uma;
+  float oxidizer_molarity;
+  float oxidizer_density_g_cm3;
 
   float fuel_residue_mol;
   float fuel_residue_g;
@@ -13,7 +19,11 @@ struct ReactionData {
 
   float main_product_g;
   float main_product_mol;
+  float main_product_volume;
   float main_product_uma;
+  float main_product_density_g_cm3;
 
   float of_ratio;
 };
+
+/* I should make the defualt reaction data a read from a file */
