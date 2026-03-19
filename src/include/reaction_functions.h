fb5b0099a175a04514f38ce23dc8e791d5cb4996
Ari <alicewulfric@gmail.com>
2026-03-19 10:19:43 -0600

    Changed some things to make it use only one struct and function properly

--- src/include/reaction_functions.h
+++ src/include/reaction_functions.h
@@ -1,2 +1,8 @@
 struct ReactionData createEmptyReactionStruct();
+
 void defineMainReactionData(struct ReactionData *main_reaction);
+
+void recalculateFromFuelMol(struct ReactionData *main_reaction, float new_fuel_ammount_mol);
+
+float massToMol(float mass, float uma);
+float molToMass(float mol, float uma);
--- src/reaction_functions.c
+++ src/reaction_functions.c
@@ -8,13 +8,18 @@
 
 struct ReactionData createEmptyReactionStruct(){
   struct ReactionData empty_reaction_data;
-
-  empty_reaction_data.fuel_g = 0; 
+  empty_reaction_data.fuel_g = 0;
+  empty_reaction_data.fuel_volume = 0; /* m3 or well, L */
   empty_reaction_data.fuel_mol = 0;
-  empty_reaction_data.fuel_uma = 0;
+  empty_reaction_data.fuel_uma = 0; /* g/mol */
+  empty_reaction_data.fuel_molarity = 0; /* M = m/L, for liquids! */
+  empty_reaction_data.fuel_density_g_cm3 = 0; /* *1000 for kg/m^3 haha */
   empty_reaction_data.oxidizer_g = 0;
+  empty_reaction_data.oxidizer_volume = 0;
   empty_reaction_data.oxidizer_mol = 0;
   empty_reaction_data.oxidizer_uma = 0;
+  empty_reaction_data.oxidizer_molarity = 0;
+  empty_reaction_data.oxidizer_density_g_cm3 = 0;
 
   empty_reaction_data.fuel_residue_mol = 0;
   empty_reaction_data.fuel_residue_g = 0;
@@ -23,7 +28,9 @@ struct ReactionData createEmptyReactionStruct(){
 
   empty_reaction_data.main_product_g = 0;
   empty_reaction_data.main_product_mol = 0;
+  empty_reaction_data.main_product_volume = 0;
   empty_reaction_data.main_product_uma = 0;
+  empty_reaction_data.main_product_density_g_cm3 = 0;
 
   empty_reaction_data.of_ratio = 0;
 
@@ -39,27 +46,30 @@ struct ReactionData createEmptyReactionStruct(){
 void defineMainReactionData(struct ReactionData *main_reaction){
   main_reaction->fuel_mol = 2;
   main_reaction->fuel_uma = 26.98;
+  main_reaction->fuel_density_g_cm3 = 2.7;
 
   main_reaction->oxidizer_mol = 6;
   main_reaction->oxidizer_uma = 36.46;
-  main_reaction->oxidizer_molarity = 6.4;
+  main_reaction->oxidizer_molarity = 6;
+  main_reaction->oxidizer_density_g_cm3 = 1.01; /* depends on concentration */
 
   main_reaction->main_product_uma = 1.008;
   main_reaction->of_ratio = 3;
 }
 
-void recalculateFromFuelMol(struct ReactionData *user_reaction, struct ReactionData *main_reaction, float new_fuel_ammount_mol){
-  user_reaction->fuel_mol = new_fuel_ammount_mol;
-  user_reaction->fuel_g = new_fuel_ammount_mol * user_reaction->fuel_uma;
-  user_reaction->fuel_volume = user_reaction->fuel_g / user_reaction->fuel_density_kg_m3;
+/* I am hardcoding it for now...*/
+void recalculateFromFuelMol(struct ReactionData *main_reaction, float new_fuel_ammount_mol){
+  main_reaction->fuel_mol = new_fuel_ammount_mol;
+  main_reaction->fuel_g = new_fuel_ammount_mol * main_reaction->fuel_uma;
+  main_reaction->fuel_volume = main_reaction->fuel_g / main_reaction->fuel_density_g_cm3;
 
-  user_reaction->oxidizer_mol = (new_fuel_ammount_mol / main_reaction->fuel_mol) * main_reaction->oxidizer_mol;
-  user_reaction->oxidizer_g = user_reaction->oxidizer_mol * user_reaction->oxidizer_uma;
-  user_reaction->oxidizer_volume = user_reaction->oxidizer_g / user_reaction->oxidizer_density_kg_m3;
+  main_reaction->oxidizer_mol = (new_fuel_ammount_mol * 3);
+  main_reaction->oxidizer_volume = (main_reaction->oxidizer_mol / main_reaction->oxidizer_molarity) * 1000; /* I want ml */
+  /*main_reaction->oxidizer_g = main_reaction->oxidizer_volume ;*/
 
-  user_reaction->main_product_mol = main_reaction->oxidizer_mol / 2; /*2HCl mol = 1 H2 mol*/
-  user_reaction->main_product_g = main_reaction->main_product_g = user_reaction->main_product_mol * user_reaction->main_product_uma;
-  user_reaction->main_product_volume = user_reaction->main_product_g / user_reaction->main_product_density_kg_m3;
+  main_reaction->main_product_mol = main_reaction->oxidizer_mol / 2; /*2HCl mol = 1 H2 mol*/
+  main_reaction->main_product_g = main_reaction->main_product_mol * main_reaction->main_product_uma;
+  main_reaction->main_product_volume = main_reaction->main_product_g / main_reaction->main_product_density_g_cm3;
   /*(void)printf("\nTo react %5.2f mol of Aluminum you need %5.3f mol of HCl\n\n", new_fuel_ammount_mol, ammount_of_oxidizer_needed);*/
 }
 
